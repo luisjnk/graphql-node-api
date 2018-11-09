6d0f7f50a16decf06cd67cecb5460f39b4828594
Luis Junqueira | Stone <ljunqueira@stone.com.br>
2018-11-09 21:02:27 -0200

    added build task into project

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,24 @@ const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
+gulp.task('scripts', ['static'], () => {
     const tsResult = tsProject.src()
         .pipe(tsProject());
 
     return tsResult.js
         .pipe(gulp.dest('dist'));
 })
+
+gulp.task('static', ['clean'], () => {
+    return gulp
+        .src(['src/**/*.json'])
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('clean', () => {
+    return gulp
+        .src('dist')
+        .pipe(clean());
+})
+
+gulp.task('build', ['clean', 'static', 'scripts'])
